f24f3eb9eebea4e409ffd78fa7ee9ccb43578994
Mohamad Khatib <116002861+mohamad1999khatib@users.noreply.github.com>
2023-05-10 17:28:02 +0300

    ( SendMessage ) AND ( UART_Receive_DMA ) WORK Final

--- H17R1/H17R1.h
+++ H17R1/H17R1.h
@@ -31,7 +31,7 @@
 
 
 /* Port-related definitions */
-#define	NumOfPorts			6
+#define	NumOfPorts			4
 
 #define P_PROG 				P2						/* ST factory bootloader UART */
 
@@ -40,34 +40,22 @@
 #define _P2 
 #define _P3 
 #define _P4 
-#define _P5 
-#define _P6
 
 /* Define available USARTs */
-#define _Usart1 1
 #define _Usart2 1
 #define _Usart3 1
-#define _Usart4 1
 #define _Usart5 1
 #define _Usart6	1
 
 
 /* Port-UART mapping */
 
-#define P1uart &huart4
+#define P1uart &huart6
 #define P2uart &huart2
 #define P3uart &huart3
-#define P4uart &huart1
-#define P5uart &huart5
-#define P6uart &huart6
-
+#define P4uart &huart5
 
 /* Port Definitions */
-#define	USART1_TX_PIN		GPIO_PIN_9
-#define	USART1_RX_PIN		GPIO_PIN_10
-#define	USART1_TX_PORT		GPIOA
-#define	USART1_RX_PORT		GPIOA
-#define	USART1_AF			GPIO_AF1_USART1
 
 #define	USART2_TX_PIN		GPIO_PIN_2
 #define	USART2_RX_PIN		GPIO_PIN_3
@@ -81,11 +69,6 @@
 #define	USART3_RX_PORT		GPIOB
 #define	USART3_AF			GPIO_AF4_USART3
 
-#define	USART4_TX_PIN		GPIO_PIN_0
-#define	USART4_RX_PIN		GPIO_PIN_1
-#define	USART4_TX_PORT		GPIOA
-#define	USART4_RX_PORT		GPIOA
-#define	USART4_AF			GPIO_AF4_USART4
 
 #define	USART5_TX_PIN		GPIO_PIN_3
 #define	USART5_RX_PIN		GPIO_PIN_2
